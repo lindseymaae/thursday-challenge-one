7ee6ef167d44aefe57fa322403286d44d08316bd
Lindsey Sandberg <sandbergy@yahoo.com>
2019-02-07 20:35:20 -0600

    more stuff

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,21 @@
 console.log('in client.js');
+
+
+$(document).ready(readyNow);
+
+function readyNow(){
+    console.log('jquery loaded');
+    $('#submitButton').on('click', addPlayer);
+}
+
+function addPlayer(){
+    
+    let playerName = $('#nameInput').val();
+    $('#tableBody').append(`
+                <tr>
+                <td> ${playerName}
+                </td>
+            </tr>
+`
+    )
+}
